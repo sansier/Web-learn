4e5630145fb4dda2f9937a3e2b76039785d5e935
毛毛狗 <maomaogou@maomaogoudeMacBook-Pro.local>
2017-04-04 15:44:20 +0800

    js练习

---
+++ js learn/course-01/js/index.js
@@ -0,0 +1,6 @@
+console.log('这是我的第一个js应用')
+var myVar =0.1 + 0.2;
+var bl = false;
+var bl2 = true;
+
+console.log(myVar);
---
+++ js learn/course-02/js/index.js
@@ -0,0 +1,19 @@
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var arr2 = [];
+arr2[0] = 'x';
+arr2[1] = 'y';
+arr2[2] = 'z';
+
+var arr3 = [[1, 2], [3, 4]]
+
+console.log(arr.length)
+console.log(arr2[1])
+console.log(arr3[1][0])
+
+for (var i = 0; i < arr.length; i++) {
+  var res = arr[i];
+  console.log(res)
+
+}
